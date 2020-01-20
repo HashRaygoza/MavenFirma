ea1ad8044430004416d1bc864df750e8107b41f4
HashRaygoza <raygoza.gomez@gmail.com>
2020-01-20 11:02:36 -0600

    v1.0
    Como aplicar, guardar y mostrar la informacion del certificado de 
    sello digital usado en el jar.

---
+++ src/main/java/mx/com/pydee/mavenfirma/Prueba.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.pydee.mavenfirma;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author David
+ */
+public class Prueba {
+    static public void main(String[] args) {
+        try {
+            Prueba prueba = new Prueba();
+            
+            String mensaje = prueba.leerCert();
+            
+            JOptionPane.showMessageDialog(null, mensaje);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Prueba.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
+        
+    }
+
+    public String leerCert() throws IOException {
+        InputStream is = getClass().getResourceAsStream("/dataCert.txt");
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String mensaje = "";
+        String cadena;
+        
+        while( (cadena = br.readLine()) != null) {
+            mensaje = mensaje + cadena + "\n";
+        }
+        
+        return mensaje;
+    }
+}
